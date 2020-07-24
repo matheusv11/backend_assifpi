0579d8a6202947a5f215c355beee535cbb73c016
matheusv11 <matheussales360@hotmail.com>
2020-07-24 19:30:44 -0300

    ok

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const cors= require('cors');
 const routes= require('./routes');
 const handle_errors= require('./configs/handle_errors')
 const verifica_fatura= require('./utils/verifica_fatura');
+const port= process.env.PORT || 3030;
 
 app.use(cors());
 app.use(express.json());
@@ -13,4 +14,4 @@ app.use(handle_errors);
 setInterval(verifica_fatura, 5000);
 
 
-app.listen(3030);
+app.listen(port);
