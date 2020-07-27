fbcf3e57d2c2657dc6845cf0d4784c2542257890
Alx-404 <alexanderreis70@gmail.com>
2020-07-27 17:30:28 -0300

    e

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ const handle_errors= require('./configs/handle_errors')
 const verifica_fatura= require('./utils/verifica_fatura');
 const port= process.env.PORT || 3030;
 
-// app.use(cors({exposedHeaders: 'total-count'})); //Set header
+app.use(cors({exposedHeaders: 'total-count'})); //Set header
 
-app.use(cors());
+//app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use(handle_errors);
