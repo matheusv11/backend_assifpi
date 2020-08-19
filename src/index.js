538aca4aa7d42cac0321444283b70b1b9a27ba93
matheusv11 <matheussales360@hotmail.com>
2020-08-18 21:49:39 -0300

    ok

--- src/index.js
+++ src/index.js
@@ -6,14 +6,14 @@ const handle_errors= require('./configs/handle_errors')
 const verifica_fatura= require('./utils/verifica_fatura');
 const port= process.env.PORT || 3030;
 
-app.use(cors({exposedHeaders: 'total-count'})); //Set header
-
+app.use(cors({exposedHeaders: 'total-count'})); //Set header //In pagination eventos
+console.log(process.env.DEV_TOKEN)
 //app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use(handle_errors);
 
-setInterval(verifica_fatura, 5000);
+setInterval(verifica_fatura, 10000);
 // 21600000
 
 app.listen(port);
