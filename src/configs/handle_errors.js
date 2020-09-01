6aca92830d712f4b7914a605813870d569464a82
matheusv11 <matheussales360@hotmail.com>
2020-09-01 03:13:18 -0300

    ok

--- src/configs/handle_errors.js
+++ src/configs/handle_errors.js
@@ -3,3 +3,5 @@ module.exports= (err, req,res,next)=>{
         return res.status(400).send({message: err.message})
     }
 }
+
+//Dar handle assim https://github.com/danielfsousa/express-rest-es2017-boilerplate/blob/032a005deaa68a06f476432f4d602917609a251a/src/api/middlewares/error.js#L36
