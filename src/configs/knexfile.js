83e04f613bdc19d9f7810aa1d643d8c96962a12f
matheusv11 <matheussales360@hotmail.com>
2020-09-13 02:48:28 -0300

    oj

--- src/configs/knexfile.js
+++ src/configs/knexfile.js
@@ -30,8 +30,9 @@ module.exports = {
     //   database: process.env.APP_DATABASE,
     // },
     connection:{
-      database: process.env.DATABASE_URL
+      database: `${process.env.DATABASE_URL}?ssl=true`,
     },
+    
     pool:{
       min:2,
       max:10
