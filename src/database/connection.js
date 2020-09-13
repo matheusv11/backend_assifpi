b9d0dddf94715bb9a30c0d8fbd17c3e926539752
matheusv11 <matheussales360@hotmail.com>
2020-09-13 02:51:40 -0300

    ok

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,6 +1,8 @@
 const knex= require('knex');
 const config= require('../configs/knexfile');
 
-const connection= knex(process.env.NODE_ENV=="production" ? config.production : config.development);
+// const connection= knex(process.env.NODE_ENV=="production" ? config.production : config.development);
+
+const connection= knex(config.development);
 
 module.exports= connection;
