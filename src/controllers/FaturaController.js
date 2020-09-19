c92c00970cc32b33cf5ae832052161665a2c2495
matheusv11 <matheussales360@hotmail.com>
2020-09-19 04:22:18 -0300

    ok

--- src/controllers/FaturaController.js
+++ src/controllers/FaturaController.js
@@ -43,8 +43,8 @@ module.exports={
             //GANHOS --------
             const meses_anos= await connection('faturas')
             .where('renovada', 1)
-            .andWhere(connection.raw(`substr(data_criacao${varchar}, 1, 4)`),ano) //Selecionar o ano
-            .select(connection.raw(`DISTINCT substr(data_criacao${varchar}, 1, 7) as meses_anos`))
+            .andWhere(connection.raw(`substr(data_criacao${varchar}, 1, 4)`),ano) //Selecionar o ano    
+            .select(connection.raw(`DISTINCT (substr(data_criacao${varchar}, 1, 7)) as meses_anos`))
             .orderBy(connection.raw(`substr(data_criacao${varchar}, 6, 2)`), 'asc') //Muito bacana //Alterar depois nos outros
             // .select(connection.raw(`substr(data_criacao, 1, 4) || '-' || substr(data_criacao, 6, 2) as meses_anos`)) //Ou object values pra remover o objeto
             //.groupBy(connection.raw(`substr(data_criacao${varchar}, 6, 2)`)) //OU SELECT PELO MES //Pode encapsular pro split teste slice //Poderia funcionar pras data talvez
@@ -63,10 +63,9 @@ module.exports={
             //GASTOS --------
             const meses_gastos= await connection('gastos')
             .andWhere(connection.raw(`substr(data${varchar}, 1, 4)`),ano) //Selecionar o ano
-            .select(connection.raw(`DISTINCT substr(data${varchar}, 1, 7)  as meses_gastos`)) //Ou object values pra remover o objeto
+            .select(connection.raw(`DISTINCT (substr(data${varchar}, 1, 7))  as meses_gastos`)) //Ou object values pra remover o objeto
             .orderBy(connection.raw(`substr(data${varchar}, 6, 2)`), 'asc') //Muito bacana //Alterar depois nos outros
-            // .groupBy(connection.raw(`substr(data${varchar}, 6, 2)`)) //OU SELECT PELO MES //Pode encapsular pro split teste slice //Poderia funcionar pras data talvez
-
+            
             const soma_gastos= meses_gastos.map(async gastos=>{
                 const [sum_gastos]= await connection('gastos')
                 .where(connection.raw(`substr(data${varchar}, 1, 7)`), gastos.meses_gastos)
